a91c6450ef891191a97de9a8091382902342a6a0
tec-sense <walkermark111989@gmail.com>
2024-06-17 10:40:15 +0530

    - LanguageStringMaster modal load

--- database/seeders/LanguageSeeder.php
+++ database/seeders/LanguageSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Language;
+use App\Models\LanguageStringMaster;
 use Illuminate\Support\Facades\DB;
 
 class LanguageSeeder extends Seeder
